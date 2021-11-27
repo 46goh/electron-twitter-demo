408e70ad8a6d220ee5d2381f46fd760f5957622c
46goh <4690goh@gmail.com>
2021-11-27 16:19:57 +0900

    enable to download google logo

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import fs from 'fs';
 
 import './index.css';
 
+const onClick = async (setSrc: (s: string) => void) => {
+  const res = await fetch('https://www.google.com/logos/doodles/2021/thanksgiving-2021-6753651837109145-2xa.gif')
+  const blob = await res.blob()
+  const url = (window.URL || window.webkitURL).createObjectURL(blob)
+  setSrc(url);
+};
+
 const App = (): JSX.Element => {
+  const [src, setSrc] = React.useState('');
   return (
     <div>
-      <h1>Hello.</h1>
+      <h1>downloader</h1>
+      <input type="file" />
+      <img src={src} />
+      <button onClick={() => onClick(setSrc)}>download</button>
     </div>
   );
 };
